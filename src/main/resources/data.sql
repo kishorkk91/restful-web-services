6346f75519d9810c8385ade325b1e78b59b0a8b5
KK05538 <kishor.kotule@devoteam.com>
2020-02-28 08:58:46 +0100

    Update changes - Post repository - Update dependency version

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,6 @@ insert into user values(3,sysdate(),'Abhishek');
 insert into post values(101,'My first post', 1);
 insert into post values(202,'My second post', 2);
 
-/*//data to test POST method JPA*/
 /*
 insert into user values(101,sysdate(),'Saket');
 insert into user values(102,sysdate(),'Anand');
